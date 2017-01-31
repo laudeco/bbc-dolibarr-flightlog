d60cdb3adca853158e5423a75532cb86829011f4
De Coninck Laurent <laurent@adlogix.eu>
2017-01-31 19:15:48 +0100

    change list auto generated

--- bbcvols_list.php
+++ bbcvols_list.php
@@ -333,7 +333,6 @@ $parameters=array();
 $reshook=$hookmanager->executeHooks('printFieldListWhere',$parameters);    // Note that $action and $object may have been modified by hook
 $sql.=$hookmanager->resPrint;
 $sql.=$db->order($sortfield,$sortorder);
-//$sql.= $db->plimit($conf->liste_limit+1, $offset);
 
 // Count total nb of records
 $nbtotalofrecords = 0;
