74d6d16f23614bb6a46eb0338542923e78ea6185
De Coninck Laurent <laurent@adlogix.eu>
2017-02-01 10:21:26 +0100

    Generate lines and documents

--- generateExpenseNote.php
+++ generateExpenseNote.php
@@ -40,12 +40,16 @@ $startDate->setDate($year, (($quarter - 1) * 3) + 1, 1);
 
 $endDate = new \DateTime();
 $endDate->setDate($year, $quarter * 3, 1);
+$endDate->add(new \DateInterval("P1M"))->sub(new \DateInterval("P1D"));
 
 $currentYear = date('Y');
 $currentQuarter = floor((date('n') - 1) / 3) + 1;
 
 $tauxRemb = isset($conf->global->BBC_FLIGHT_LOG_TAUX_REMB_KM) ? $conf->global->BBC_FLIGHT_LOG_TAUX_REMB_KM : 0;
 $flightYears = getFlightYears();
+
+$object = new ExpenseReport($db);
+$vatrate = "0.000";
 
 // Access control
 if (!$conf->expensereport->enabled || !$user->rights->flightLog->flight->billable) {
@@ -69,16 +73,72 @@ print load_fiche_titre("Générer note de frais");
 if ($action == EXPENSE_REPORT_GENERATOR_ACTION_GENERATE) {
 
     if ($year < $currentYear || ($year == $currentYear && $quarter < $currentQuarter)) {
-        $object = new ExpenseReport($db);
-        $object->date_debut = $startDate->format("Y-m-d");
-        $object->date_fin = $endDate->format("Y-m-d");
 
-        $object->fk_statut = 1;
-        $object->fk_user_validator = $user->id;
-        $object->note_public = "TEST";
-        $object->note_private = GETPOST('note_private');
+        $missions = bbcKilometersByQuartil($year);
 
-        $result = $object->create($user);
+        foreach($missions as $currentMissionUserId => $currentMission){
+
+            $expenseNoteUser = new User($db);
+            $expenseNoteUser->id = $currentMissionUserId;
+
+            $object = new ExpenseReport($db);
+            $object->date_debut = $startDate->format("Y-m-d");
+            $object->date_fin = $endDate->format("Y-m-d");
+
+            $object->fk_statut = 1;
+            $object->fk_user_validator = GETPOST("fk_user_validator", 'int');
+            $object->note_public = GETPOST('public_note', 'alpha');
+            $object->note_private = GETPOST('private_note','alpha');
+
+            $expenseNoteId = $object->create($expenseNoteUser);
+
+            $object_ligne = new ExpenseReportLine($db);
+            $object_ligne->comments = "Kilometres réalisé pour le club";
+            $object_ligne->qty = $currentMission["quartil"][$quarter]["km"];
+            $object_ligne->value_unit = $tauxRemb;
+
+            $object_ligne->date = $endDate->format("Y-m-d");
+
+            $object_ligne->fk_c_type_fees = 2;
+            $object_ligne->fk_expensereport = $expenseNoteId;
+            $object_ligne->fk_projet = '';
+
+            $object_ligne->vatrate = price2num($vatrate);
+
+            $tmp = calcul_price_total($object_ligne->qty, $object_ligne->value_unit, 0, $vatrate, 0, 0, 0, 'TTC', 0, 0, '');
+            $object_ligne->total_ttc = $tmp[2];
+            $object_ligne->total_ht = $tmp[0];
+            $object_ligne->total_tva = $tmp[1];
+
+            $resultLine = $object_ligne->insert();
+
+            $object_ligne = new ExpenseReportLine($db);
+            $object_ligne->comments = "Mission réalisé pour le club";
+            $object_ligne->qty = $currentMission["quartil"][$quarter]["flight"];
+            $object_ligne->value_unit = 35;
+
+            $object_ligne->date = $endDate->format("Y-m-d");
+
+            $object_ligne->fk_c_type_fees = 8;
+            $object_ligne->fk_expensereport = $expenseNoteId;
+            $object_ligne->fk_projet = '';
+
+            $object_ligne->vatrate = price2num($vatrate);
+
+            $tmp = calcul_price_total($object_ligne->qty, $object_ligne->value_unit, 0, $vatrate, 0, 0, 0, 'TTC', 0, 0, '');
+            $object_ligne->total_ttc = $tmp[2];
+            $object_ligne->total_ht = $tmp[0];
+            $object_ligne->total_tva = $tmp[1];
+
+            $resultLine = $object_ligne->insert();
+
+            $object->setValidate($user);
+            $object->setDocModel($user, "standard");
+            $object->generateDocument($object->modelpdf, $langs);
+
+        }
+
+
         if ($result > 0) {
             dol_htmloutput_mesg("Notes de frais créées");
         }else{
@@ -117,8 +177,6 @@ dol_fiche_head($tabLinks, "tab_".$year);
         <!-- action -->
         <input type="hidden" name="action" value="<?= EXPENSE_REPORT_GENERATOR_ACTION_GENERATE ?>">
 
-
-
         <?php printBbcKilometersByQuartil(bbcKilometersByQuartil($year), $tauxRemb); ?>
 
         <!-- Quarter -->
@@ -130,6 +188,14 @@ dol_fiche_head($tabLinks, "tab_".$year);
             <option value="3" <?= ($year == $currentYear && $currentQuarter <= 3) ? 'disabled="disabled"': "" ?>>3</option>
             <option value="4" <?= ($year == $currentYear && $currentQuarter <= 4) ? 'disabled="disabled"' : "" ?>>4</option>
         </select>
+        <br/>
+
+        <!-- Validator -->
+        <label><?= $langs->trans("Validateur de la note de frais")?></label><br/>
+        <?php
+            $include_users = $object->fetch_users_approver_expensereport();
+            print $form->select_dolusers($user->id,"fk_user_validator",1,"",0,$include_users);
+        ?>
         <br/>
 
         <!-- Public note -->
@@ -144,7 +210,11 @@ dol_fiche_head($tabLinks, "tab_".$year);
 
         <!-- Validate expense note -->
         <label><?= $langs->trans("Valider les notes de frais : "); ?></label>
-        <input type="checkbox" value="1" name="validate"/>
+        <input type="checkbox" value="2" name="validate" checked/>
+        <br/>
+
+        <label><?= $langs->trans("Ligne note de frais"); ?></label>
+        <input type="text"  name="line_description"/>
         <br/>
 
         <button class="butAction" type="submit">Générer</button>
