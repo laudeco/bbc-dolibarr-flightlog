95ae497749226c9251a8819d570e5f22b53fc14f
De Coninck Laurent <laurent@adlogix.eu>
2017-12-03 16:58:15 +0100

    clean module

--- core/modules/modFlightLog.class.php
+++ core/modules/modFlightLog.class.php
@@ -120,15 +120,8 @@ class modFlightLog extends DolibarrModules
         // Key text used to identify module (for permissions, menus, etc...)
         $this->rights_class = 'flightlog';
 
-        // Family can be 'crm','financial','hr','projects','products','ecm','technic','interface','other'
-        // It is used to group modules by family in module setup page
         $this->family = "Belgian Balloon Club";
-        // Module position in the family
         $this->module_position = 500;
-        // Gives the possibility to the module, to provide his own family info and position of this family (Overwrite $this->family and $this->module_position. Avoid this)
-        //$this->familyinfo = array('myownfamily' => array('position' => '001', 'label' => $langs->trans("MyOwnFamily")));
-
-        // Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
         $this->name = preg_replace('/^mod/i', '', get_class($this));
         // Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
         $this->description = "Pilots flight log";
