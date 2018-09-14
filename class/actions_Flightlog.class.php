3c5c0ff09a1e6187c3b318160319e8660160b6b5
De Coninck Laurent <laurent@adlogix.eu>
2018-09-14 15:48:44 +0200

    more data in the result of link to flight

--- class/actions_Flightlog.class.php
+++ class/actions_Flightlog.class.php
@@ -28,8 +28,8 @@ class ActionsFlightlog
 
         $this->results["flightlog"] = [
             'label' => $langs->trans("Search flight"),
-            'text'  => $langs->trans("Search flight"),
-            'url'   => DOL_URL_ROOT . '/flightlog/list.php?mainmenu=flightlog&sall='.$searchInfo['search_boxvalue']
+            'text' => $langs->trans("Search flight"),
+            'url' => DOL_URL_ROOT . '/flightlog/list.php?mainmenu=flightlog&sall=' . $searchInfo['search_boxvalue']
         ];
     }
 
@@ -38,8 +38,34 @@ class ActionsFlightlog
      * @param $object
      * @param $action
      */
-    public function showLinkToObjectBlock($parameter, $object, $action){
-        $this->results["flightlog_bbcvols"]= array('enabled'=>1, 'perms'=>1, 'label'=>'LinkToFlight', 'sql'=>"SELECT f.idBBC_vols as rowid, CONCAT('(ID : ',f.idBBC_vols, ') -',f.date, '-',f.lieuD, ' ; ', f.lieuA) as ref FROM ".MAIN_DB_PREFIX."bbc_vols as f WHERE YEAR(f.date) = (YEAR(NOW())) ORDER BY date DESC");
+    public function showLinkToObjectBlock($parameter, $object, $action)
+    {
+        $this->results["flightlog_bbcvols"] = [
+            'enabled' => 1,
+            'perms' => 1,
+            'label' => 'LinkToFlight',
+            'sql' => $this->getSqlForLink(),
+        ];
 
+    }
+
+    /**
+     * @return string
+     */
+    private function getSqlForLink()
+    {
+        $sql = "SELECT ";
+        $sql .= " f.idBBC_vols as rowid ";
+        $sql .= ", f.cost as total_ht ";
+        $sql .= ", CONCAT('(ID : ',f.idBBC_vols, ') - ' ,f.date, ' - ',f.lieuD, ' => ', f.lieuA) as ref ";
+
+        $sql .= " FROM ";
+        $sql .= MAIN_DB_PREFIX . "bbc_vols as f ";
+
+        $sql .= "WHERE YEAR(f.date) = (YEAR(NOW())) ";
+        $sql .= " AND f.fk_type IN (1,2) ";
+        $sql .= " ORDER BY date DESC";
+
+        return $sql;
     }
 }
