3c9458360f439dddd2840b5c07c64a551685bead
Laurent De Coninck <lau.deconinck@gmail.com>
2021-10-30 12:42:33 +0200

    :ambulance: fix right for menu entry

--- core/modules/modFlightLog.class.php
+++ core/modules/modFlightLog.class.php
@@ -417,7 +417,7 @@ class modFlightLog extends DolibarrModules
             'langs' => 'mylangfile',
             'position' => 110,
             'enabled' => '1',
-            'perms' => '$user->rights->flightlog->vol->financial',
+            'perms' => '',
             'target' => '',
             'user' => 2
         );
