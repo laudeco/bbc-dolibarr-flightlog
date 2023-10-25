22bab6961eeccdf3d30b0c74a5ad709ec107ba96
Laurent De Coninck <laurent.deconinck@alphanetworks.tv>
2023-10-25 14:43:30 +0200

    fix the update of the flight

--- card.php
+++ card.php
@@ -364,7 +364,7 @@ if (($user->rights->flightlog->vol->edit || ($user->rights->flightlog->vol->add 
         ?>
     </div>
 
-    <form class="flight-form js-form" name='add' action="card.php?rowid=<?php echo $object->getId(); ?>" method="POST">
+    <form class="flight-form js-form" name='add' action="card.php?id=<?php echo $object->getId(); ?>" method="POST">
         <input type="hidden" name="action" value="update"/>
         <input type="hidden" name="token" value="<?php echo newToken();?>"/>
 
